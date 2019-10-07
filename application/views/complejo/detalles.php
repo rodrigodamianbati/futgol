12bd993d3d6b6f86b28629735039a8410dfe207f
Rodrigo <r.batillier@gmail.com>
2019-10-07 14:49:01 -0300

    Revert "Detalles complejo."
    This reverts commit 3c492c92a649bbd7ec50e49f62f5c943f2d4a965.

--- application/views/complejo/detalles.php
+++ application/views/complejo/detalles.php
@@ -66,9 +66,11 @@
 									<span class="label label-default">Precio por </span>
 									<span class="label label-success"><b>$</b></span>
 									</h3>
-									<p>Fecha: <?=$this->session->datos->fecha?> </p>
-									<p>Hora: <?=$this->session->datos->hora?></p>
-									<p>Cantidad de jugadores: <?=$this->session->datos->jugadores?></p>				
+									<p>
+									Desde el
+									</p>
+								
+												
 									<?php if (count($servicios)>0){?>
 									<h4>Servicios</h4>
 									<div class="row contact-info-wrap">
